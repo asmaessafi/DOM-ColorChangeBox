c6301752a513cab88774ebef81c30a5a5f213f73
asma <asmaessafi137@gmail.com>
2024-11-17 10:47:35 +0100

    the old way

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded',function(){
     var box=document.getElementById('color-box');
     var btn=document.getElementById('change-color-btn');
 
@@ -7,8 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let red=Math.ceil(Math.random()*255)
     let green=Math.ceil(Math.random()*255)
     let blue=Math.ceil(Math.random()*255)
-        return box.style.backgroundColor=`rgb(${red},${green},${blue})`;
-    
+        return box.style.backgroundColor="rgb("+red+','+green+','+blue+')'
+    // const hex = Math.floor(Math.random() * 16777215).toString(16);
+    // return box.style.backgroundColor='#'+hex.padStart(6, '0');
     };
 
     btn.addEventListener('click',getRandomColor);
